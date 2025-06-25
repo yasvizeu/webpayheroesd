c50230832e8ba6cbc644208d45ba942bc97d07f2
yasvizeu <yasminvizeusantana@yahoo.com>
2025-06-25 11:36:25 -0300

    create-hero component e estilo

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroesComponent } from '../components/heroes/heroes.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
 @NgModule({
   declarations: [HeroesComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   exports: [HeroesComponent]
 })
